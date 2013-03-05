85b9027b21663a1a50dcb1656ebd76ccf5a75407
Brendan Russo <brendanrusso27@gmail.com>
2013-03-06 09:13:47 +1000

    Prac 1 is complete and trying to push using visual studio.

---
+++ ComputeAverageAge/ComputeAverageAge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeAverageAge
+{
+    class Program
+    {
+        const Boolean DEBUG = true;
+        static void Main(string[] args)
+        {
+            float sum = 0;
+            int amount = 5;
+            ArrayList ages = new ArrayList();
+
+            Console.WriteLine("Hello world!");
+
+            //Gets 5 ages from the user
+            for (int i = 0; i < amount; i++)
+            {
+                Console.Write("Enter age: ");
+                int age = int.Parse(Console.ReadLine());
+                sum += age;
+                ages.Add(age);
+                if (DEBUG)
+                {
+                    Console.WriteLine("sum is: " + sum);
+                    Console.WriteLine("current age is: " + ages[i]);
+                }
+            }
+
+            //sorts the ages to find the mean
+            ages.Sort();          
+            float mean = sum / 5.0f;
+            Console.WriteLine("mean is " + mean);
+            Console.WriteLine("median is " + ages[amount/2]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ComputeAverageAge/GUIComputerAverageAge/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Collections;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUIComputerAverageAge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           // Result.Text = "Hello World";
+        }
+
+        private void btnCompute_Click(object sender, RoutedEventArgs e)
+        {
+            double averageAge = getAverageAge(AgeValues.Text);
+            Result.Text = "Result: " + averageAge;
+        }
+        private double getAverageAge(String listOfAges){
+            double totalAge = 0;
+            double sum = 0;
+            String[] arrayOfAges = listOfAges.Split(' ');
+
+            foreach (String age in arrayOfAges)
+            {
+                try
+                {
+                    totalAge += int.Parse(age);
+                    ++sum;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("invalid charater");
+                }
+            }
+
+            return totalAge / sum;
+        }
+    }
+}
