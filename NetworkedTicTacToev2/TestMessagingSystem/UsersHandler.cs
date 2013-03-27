78a01b3f0205e973f00df7e4de8e9dc99ce3a019
Brendan Russo <brendanrusso27@gmail.com>
2013-03-27 19:26:07 +1000

    Prac 4 finished was a painful as I thought it would be. These instructions are terrible and ended up just looking at the state diagram to find out what to do.

--- NetworkedTicTacToev2/TestMessagingSystem/UsersHandler.cs
+++ NetworkedTicTacToev2/TestMessagingSystem/UsersHandler.cs
@@ -20,35 +20,152 @@ namespace TestMessagingSystem
 
         public string handleRequest(string request)
         {
-            string addUsersRegex = @"^ADD_USER,[A-Za-z]+,[0-9]+$";
+            string action = request.Split(',')[0];
             string response = "ERROR";
             switch (state)
             {
                 case State.WAIT:
-                    if (Regex.Match(request, addUsersRegex).Success)
+                    //Checks the state
+                    if (action.Equals("ADD_USER"))
                     {
-                        state = State.ADD_USERS;
-                        response = "SUCCESS";
-                        
-                        System.Diagnostics.Debug.WriteLine("State: ADD_USERS");
-                        //System.Diagnostics.Debug.WriteLine("user: " + user);
-
+                        //Right state checks for right format
+                        if (Regex.Match(request, @"^ADD_USER,[A-Za-z]+$").Success)
+                        {
+                            string name = request.Split(',')[1];
+                            //Checks if user exists
+                            if (canAddUser(name))
+                            {
+                                //Adds user, changes respose and state
+                                addUser(name);
+                                response = "DONE";
+                                state = State.ADD_USERS;
+                            }
+                                //user already there
+                            else
+                            {
+                                response = "ERROR_USER_EXISTS";
+                            }
+                        }
+                    }
+                        //Checks if score
+                    else if (action.Equals("ADD_SCORE"))
+                    {
+                        //Checks right format
+                        if (Regex.Match(request, @"^ADD_SCORE,[A-Za-z]+,[0-9]+$").Success)
+                        {
+                            string name = request.Split(',')[1];
+                            int movecount = Int32.Parse(request.Split(',')[2]);
+                            //Sees if it can update the score
+                            if (updateScore(name, movecount))
+                            {
+                                //Can update changes state
+                                response = "DONE";
+                                state = State.ADD_SCORES;
+                            }
+                            else
+                            {
+                                response = "ERROR_UNKNOWN_USER";
+                            }
+                        }
                     }
                     break;
                 case State.ADD_USERS:
-                    if (Regex.Match(request, addUsersRegex).Success)
+                    //Checks the state for add user
+                    if (action.Equals("ADD_USER"))
                     {
-                        response = "SUCCESS";
-                        string user = request.Split(',')[1];
-                        System.Diagnostics.Debug.WriteLine("State: ADD_USERS");
-                        System.Diagnostics.Debug.WriteLine("user: " + user);
-
+                        //Right state checks for right format
+                        if (Regex.Match(request, @"^ADD_USER,[A-Za-z]+$").Success)
+                        {
+                            string name = request.Split(',')[1];
+                            //Checks if user exists
+                            if (canAddUser(name))
+                            {
+                                addUser(name);
+                                response = "DONE";
+                            }
+                            //user already there
+                            else
+                            {
+                                response = "ERROR_USER_EXISTS";
+                            }
+                        }
+                    }
+                        //Checks if end add users
+                    else if(action.Equals("END_ADD_USERS"))
+                    {
+                        response = "OKAY";
+                        state = State.WAIT;
                     }
                     break;
                 case State.ADD_SCORES:
+                    //checks if it can add score
+                    if (action.Equals("ADD_SCORE"))
+                    {
+                        //Right state checks for right format
+                        if (Regex.Match(request, @"^ADD_SCORE,[A-Za-z]+,[0-9]+$").Success)
+                        {
+                            string name = request.Split(',')[1];
+                            int movecount = Int32.Parse(request.Split(',')[2]);
+                            //checks if it can update
+                            if (updateScore(name, movecount))
+                            {
+                                response = "DONE";
+                                state = State.ADD_SCORES;
+                            }
+                            else
+                            {
+                                response = "ERROR_UNKNOWN_USER";
+                            }
+                        }
+                    }
+                        //Ending adding score
+                    else if(action.Equals("END_ADD_SCORES"))
+                    {
+                        response = "OKAY";
+                        state = State.WAIT;
+                    }
                     break;
             }
             return response;
+        }
+        private void addUser(string name)
+        {
+            User newUser = new User();
+            newUser.name = name;
+            newUser.scores = new List<Score>();
+            users.Add(newUser);
+        }
+
+        private bool updateScore(string name, int movecount)
+        {
+            bool exists = false;
+            for (int i = 0; i < users.Count; ++i)
+            {
+                if (users[i].name.Equals(name))
+                {
+                    Score score = new Score();
+                    score.moveCount = movecount;
+                    users[i].scores.Add(score);
+                    exists = true;
+                    break;
+                }
+            }
+            return exists;
+        }
+
+        private bool canAddUser(string name)
+        {
+            bool exists = true;
+            //Loops through all users to see if name exists 
+            foreach (User user in users)
+            {
+                if (user.name.ToLower().Equals(name))
+                {
+                    exists = false;
+                    break;
+                }
+            }
+            return exists;
         }
     }
 }
